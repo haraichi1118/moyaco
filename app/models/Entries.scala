2a75e173c81a6809a83cb1a561dd95ad7f966706
築城賢司 <tsuiki@shinqafactory.com>
2013-12-03 08:39:43 +0900

    setting slick

--- app/models/Entries.scala
+++ app/models/Entries.scala
@@ -1,11 +1,15 @@
 package models
 
 import org.joda.time.DateTime
-import play.api.db.DB
-import anorm._
-import anorm.SqlParser._
-import models.Entry
-import anorm.~
+import play.api.db._
+import play.api.Play.current
+
+import scala.slick.driver.MySQLDriver.simple._
+import scala.slick.jdbc._ // --(*) これが超大事！！
+import Database.threadLocalSession
+
+import org.apache.commons.dbcp.BasicDataSource
+import javax.sql.DataSource
 
 case class Entry(id: Long, userId: Long, content: String, displayFlg: Boolean, createdAt: DateTime, updatedAt: DateTime)
 
@@ -18,22 +22,25 @@ case class Entry(id: Long, userId: Long, content: String, displayFlg: Boolean, c
  */
 object Entries {
 
-  val entry = {
-    get[Long]("id") ~ get[Long]("userId") ~ get[String]("content") ~ get[Boolean]("displayFlg") ~
-      get[org.joda.time.DateTime]("createdAt") ~ get[org.joda.time.DateTime]("updatedAt") map {
-      case id~userId~content~displayFlg~createdAt~updatedAt => Entry(id, userId, content, displayFlg, createdAt, updatedAt)
-    }
-  }
 
-  // すべての記事を取得
-  def all(): List[Entry] = DB.withConnection { implicit c =>
-    SQL("select * from entry").as(entry *)
-  }
+  object
 
-  // 公開状態の記事を取得
-  def findByDisplayFlg(): List[Entry] = DB.withConnection { implicit c =>
-    SQL("select * from entry where display_flg = 0").as(entry *)
-  }
+//  val entry = {
+//    get[Long]("id") ~ get[Long]("userId") ~ get[String]("content") ~ get[Boolean]("displayFlg") ~
+//      get[org.joda.time.DateTime]("createdAt") ~ get[org.joda.time.DateTime]("updatedAt") map {
+//      case id~userId~content~displayFlg~createdAt~updatedAt => Entry(id, userId, content, displayFlg, createdAt, updatedAt)
+//    }
+//  }
+//
+//  // すべての記事を取得
+//  def all(): List[Entry] = DB.withConnection { implicit c =>
+//    SQL("select * from entry").as(entry *)
+//  }
+//
+//  // 公開状態の記事を取得
+//  def findByDisplayFlg(): List[Entry] = DB.withConnection { implicit c =>
+//    SQL("select * from entry where display_flg = 0").as(entry *)
+//  }
 
 
 }
--- build.sbt
+++ build.sbt
@@ -5,7 +5,12 @@ version := "1.0-SNAPSHOT"
 libraryDependencies ++= Seq(
   jdbc,
   anorm,
-  cache
+  cache,
+  "mysql" % "mysql-connector-java" % "5.1.18",
+  "com.typesafe.slick" %% "slick" % "1.0.1",
+  "com.typesafe.play" %% "play-slick" % "0.5.0.8",
+  "commons-dbcp" % "commons-dbcp" % "1.4",
+  "com.github.tototoshi" %% "slick-joda-mapper" % "0.4.0"
 )     
 
 play.Project.playScalaSettings
